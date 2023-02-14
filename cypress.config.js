352dc200de7a8bd4f1f405aff3d1456b576e9b9d
MayconVitor <62214657+MayconVitor@users.noreply.github.com>
2023-02-14 00:44:12 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  
   projectId: 'ky8zo5',
   e2e: { 
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     baseUrl:'https://automationexercise.com',
-
     env:{
       apiUrl:'https://automationexercise.com/api'
-    },
-
-    viewportWidth: 1920,viewportHeight: 1080
-  },
+    }
+  }
 });
