b66a25baed24573f2595129fd4c1dc4d1fe3d97a
maycon.vitor_accenture@prestador.globo <Acc.06120905>
2023-02-14 01:13:03 -0300

    update  version cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'ky8zo5',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-
     baseUrl:'https://automationexercise.com',
-
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     env:{
       apiUrl:'https://automationexercise.com/api'
-    },
-
-    viewportWidth: 1920,viewportHeight: 1080,
+    }, 
   },
 });
